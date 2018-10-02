d7291ba397129e2c4317060d8c8b972bdc1c00cb
Facundo Flores <flores.facundogabriel@gmail.com>
2018-10-02 17:10:43 +0200

    Add countEven function

--- src/countEven.js
+++ src/countEven.js
@@ -1,3 +1,13 @@
-export const countEven = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const countEven = values => {
+  if (!values || !values.length) {
+    return 0;
+  }
+
+  return values.reduce((acc, value) => {
+    if (Number.isInteger(value) && value % 2 === 0) {
+      return acc + 1;
+    }
+
+    return acc;
+  }, 0);
 };
